0e70e8c1a38697872d04f24281e452fa299c4c40
HivaSteel <info@dsscrane.com>
2026-08-02 12:43:10 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,104 @@
 async function loadWebsite() {
 
-    const response = await fetch("data/products.json");
+    const siteResponse = await fetch("data/site.json");
+    const site = await siteResponse.json();
 
-    const products = await response.json();
 
-    console.log(products);
-    console.log(Array.isArray(products));
+    const productsResponse = await fetch("data/products.json");
+    const products = await productsResponse.json();
+
+
+    console.log("Products:", products);
+    console.log("Is array:", Array.isArray(products));
+
+
+    // Update company information
+
+    document.querySelector("h1").textContent =
+        site.companyName;
+
+
+    document.querySelector("header p").textContent =
+        site.tagline;
+
+
+    document.querySelector(".buttons a:first-child").href =
+        "tel:" + site.phone;
+
+
+    document.querySelector(".buttons a:nth-child(2)").href =
+        "https://wa.me/" + site.whatsapp;
+
+
+
+    // Update products
+
+    const container = document.getElementById("products");
+
+    container.innerHTML = "";
+
+
+    products.forEach(item => {
+
+
+        const card = document.createElement("div");
+
+        card.className = "card";
+
+
+        card.innerHTML = `
+
+            <h3>
+                ${item.category} ${item.grade}
+            </h3>
+
+
+            <p>
+            <strong>ضخامت:</strong>
+            ${item.thickness} میلی‌متر
+            </p>
+
+
+            <p>
+            <strong>نوع:</strong>
+            ${item.form}
+            </p>
+
+
+            <p>
+            <strong>ابعاد:</strong>
+            ${item.dimensions}
+            </p>
+
+
+            <p>
+            <strong>کارخانه:</strong>
+            ${item.factory}
+            </p>
+
+
+            <span class="badge">
+                🟢 موجود
+            </span>
+
+
+            <br><br>
+
+
+            <a href="tel:${site.phone}">
+                📞 استعلام قیمت
+            </a>
+
+        `;
+
+
+        container.appendChild(card);
+
+
+    });
+
 
 }
+
 
 loadWebsite();
