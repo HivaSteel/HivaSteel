4897ada77cbe6a5444cc1d0281c625754661a498
HivaSteel <info@dsscrane.com>
2026-08-02 12:39:51 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,12 @@
-products.forEach(item => {
+async function loadWebsite() {
 
-container.innerHTML += `
+    const response = await fetch("data/products.json");
 
-<div class="card">
+    const products = await response.json();
 
-<h3>
-${item.category} ${item.grade}
-</h3>
+    console.log(products);
+    console.log(Array.isArray(products));
 
-<p>
-<strong>ضخامت:</strong>
-${item.thickness} میلی‌متر
-</p>
+}
 
-<p>
-<strong>نوع:</strong>
-${item.form}
-</p>
-
-<p>
-<strong>ابعاد:</strong>
-${item.dimensions}
-</p>
-
-<p>
-<strong>کارخانه:</strong>
-${item.factory}
-</p>
-
-<span class="badge">
-🟢 موجود
-</span>
-
-<p style="margin-top:20px">
-
-<a href="tel:${site.phone}">
-📞 استعلام قیمت
-</a>
-
-</p>
-
-</div>
-
-`;
-
-});
+loadWebsite();
